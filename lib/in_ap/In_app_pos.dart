d62cff49e3f32653e8f0035b82d23ff565e5f33b
ebuka.miracle <DF_Ebuka.Miracle@zenithbank.com>
2023-08-01 10:11:56 +0100

    fontasome change

--- lib/in_ap/In_app_pos.dart
+++ lib/in_ap/In_app_pos.dart
@@ -6,7 +6,7 @@ import '../screens/in_app_money_pad.dart';
 import '../utils/transaction_type.dart';
 
 class InAppPOS {
-  start(BuildContext context, {Map? terminalInfo}) {
+  start(BuildContext context, String userId) {
     Navigator.push(
         context,
         MaterialPageRoute(
@@ -18,7 +18,8 @@ class InAppPOS {
                         MaterialPageRoute(
                             builder: (_) => InAppPOSAccountSelectionScreen(
                                   onSelectAccountType: (accountType) {
-                                    EpSoftposPlugin().ePayment(amount);
+                                    EpSoftposPlugin().ePayment(
+                                        amount: amount, userID: userId);
                                   },
                                 )));
                   },
