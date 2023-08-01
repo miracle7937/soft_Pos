44328533ddbea7c92475436d0ad7b68f25177852
ebuka.miracle <DF_Ebuka.Miracle@zenithbank.com>
2023-08-01 10:42:45 +0100

    update input pad................................

--- lib/in_ap/In_app_pos.dart
+++ lib/in_ap/In_app_pos.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../../screens/amount_pad_screen.dart';
 import '../ep_softpos_plugin.dart';
-import '../screens/in_app_money_pad.dart';
 import '../utils/transaction_type.dart';
 
 class InAppPOS {
@@ -13,15 +12,15 @@ class InAppPOS {
             builder: (_) => PosAmountScreen(
                   transactionType: TransactionType.purchase,
                   onSelectAmount: (amount) {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (_) => InAppPOSAccountSelectionScreen(
-                                  onSelectAccountType: (accountType) {
-                                    EpSoftposPlugin().ePayment(
-                                        amount: amount, userID: userId);
-                                  },
-                                )));
+                    EpSoftposPlugin().ePayment(amount: amount, userID: userId);
+                    // Navigator.push(
+                    //     context,
+                    //     MaterialPageRoute(
+                    //         builder: (_) => InAppPOSAccountSelectionScreen(
+                    //               onSelectAccountType: (accountType) {
+                    //
+                    //               },
+                    //             )));
                   },
                 )));
   }
